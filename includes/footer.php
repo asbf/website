745dbb767b9aa46b40992145102983f3ab7fb191
MKody <im@kdy.ch>
2015-03-04 18:58:37 +0100

    Un peu de PHP

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+
+      </div> <!-- //.row -->
+
+      <div class="clearfix"></div>
+
+      <div id="footer">
+        <a href="/ressources/">Logo et ressources</a> - Actions Solidaires Brony Francophone 2015
+      </div> <!-- //#footer -->
+
+    </div> <!-- //#container -->
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,49 +1,4 @@
-<!DOCTYPE html>
-<!-- http://bootswatch.com/cosmo/ -->
-<html lang="fr">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>Actions Solidaires Brony Francophone</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/css/bootstrap.css" media="screen">
-    <link rel="stylesheet" href="/css/custom.css" media="screen">
-    <link rel="stylesheet" href="/css/ionicons.min.css" media="screen">
-
-    <!-- Madness! -->
-    <link rel="apple-touch-icon" sizes="57x57" href="/icon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="/icon/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="/icon/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="/icon/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="/icon/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="/icon/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="/icon/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="/icon/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="/icon/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="/icon/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/icon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="/icon/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/icon/favicon-16x16.png">
-    <link rel="manifest" href="/icon/manifest.json">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="/icon/ms-icon-144x144.png">
-    <meta name="theme-color" content="#ffffff">
-    <!-- //Madness! -->
-  </head>
-
-  <body>
-
-    <div class="container">
-
-      <div class="page-header" id="banner">
-        <div class="row">
-          <div class="col-lg-8 col-md-7 col-sm-6">
-            <a href="/"><img class="img-responsive" src="/img/header.png" alt="Logo ASBF"></a>
-          </div>
-        </div>
-      </div>
-
-      <div class="row">
-
+<?php require './includes/header.php'; ?>
         <!-- Barre latérale -->
         <div class="col-lg-3">
           <div class="bs-component">
@@ -174,15 +129,4 @@
           </div> <!-- //.bs-component -->
         </div> <!-- //.col-lg-9 -->
 
-      </div> <!-- //.row -->
-
-      <div class="clearfix"></div>
-
-      <div id="footer">
-        <a href="/ressources/">Logo et ressources</a> - Dernière mise à jour de la page le 3 mars 2015
-      </div> <!-- //#footer -->
-
-    </div> <!-- //#container -->
-
-</body>
-</html>
+<?php require './includes/footer.php'; ?>
