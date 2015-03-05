8fef30d678444ba9313f2e03151569c3f5df626c
heuzef <contact@heuzef.com>
2015-03-05 14:54:18 +0100

    Suppression des ressources (migration sur le wiki)

--- includes/footer.php
+++ includes/footer.php
@@ -4,7 +4,7 @@
       <div class="clearfix"></div>
 
       <div id="footer">
-        <a href="/ressources/">Logo et ressources</a> - Actions Solidaires Brony Francophone 2015
+        Actions Solidaires Brony Francophone 2015
       </div> <!-- //#footer -->
 
     </div> <!-- //#container -->
