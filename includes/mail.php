6000a48db8fe43f6758378e19018770bab67e205
MKody <im@kdy.ch>
2015-04-10 12:58:43 +0000

    Seconde partie pour le contact, terminé et fixé

--- includes/mail.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$mailUser = "contact@asbf";
-$mailPass = "";
-$mailHost = "asbf.fr";
-$mailPort = "465";
--- mu/index.php
+++ mu/index.php
@@ -1,7 +1,11 @@
-<?php require '../includes/header.php';
-require '../includes/nav.php'; ?>
+<?php 
+require '../includes/header.php';
+require '../includes/nav.php';
 
-
+if(isset($_GET['success'])) $success = 1;
+if(isset($_GET['bot'])) $bot = 1;
+if(!empty($_GET['error'])) $error = urldecode($_GET['error']);
+?>
 
 						<div class="modal" id="proposer" aria-hidden="true">
 							<div class="modal-dialog">
@@ -17,49 +21,49 @@ require '../includes/nav.php'; ?>
 												<div class="form-group">
 													<label for="nom" class="col-lg-2 control-label">Nom, Prénom</label>
 													<div class="col-lg-10">
-														<input type="text" class="form-control" id="nom" name="nom" placeholder="Votre nom et prénom OU votre pseudo">
+														<input type="text" class="form-control" id="nom" name="nom" placeholder="Votre nom et prénom OU votre pseudo" required>
 													</div>
 												</div>
 
 												<div class="form-group">
 													<label for="email" class="col-lg-2 control-label">Email</label>
 													<div class="col-lg-10">
-														<input type="email" class="form-control" id="email" name="email" placeholder="Votre email, à vous, pour vous recontacter">
+														<input type="email" class="form-control" id="email" name="email" placeholder="Votre email, à vous, pour vous recontacter" required>
 													</div>
 												</div>
 
 												<div class="form-group">
 													<label for="date" class="col-lg-2 control-label">Date</label>
 													<div class="col-lg-10">
-														<input type="date" class="form-control" id="date" name="date" placeholder="31/12/2015">
+														<input type="date" class="form-control" id="date" name="date" placeholder="31/12/2015" autocomplete="off" required>
 													</div>
 												</div>
 
 												<div class="form-group">
 													<label for="lieu" class="col-lg-2 control-label">Lieu</label>
 													<div class="col-lg-10">
-														<input type="text" class="form-control" id="lieu" name="lieu" placeholder="Lieu de l'évènement">
+														<input type="text" class="form-control" id="lieu" name="lieu" placeholder="Lieu de l'évènement" autocomplete="off" required>
 													</div>
 												</div>
 
 												<div class="form-group">
 													<label for="asso" class="col-lg-2 control-label">Nom de l'action</label>
 													<div class="col-lg-10">
-														<input type="text" class="form-control" id="asso" name="asso" placeholder="Association aidée ou action rejointe">
+														<input type="text" class="form-control" id="asso" name="asso" placeholder="Association aidée ou action rejointe" autocomplete="off" required>
 													</div>
 												</div>
 
 												<div class="form-group">
 													<label for="website" class="col-lg-2 control-label">Site web</label>
 													<div class="col-lg-10">
-														<input type="text" class="form-control" id="website" name="website" placeholder="Site de l'association ou de l'action">
+														<input type="text" class="form-control" id="website" name="website" placeholder="Site de l'association ou de l'action" autocomplete="off">
 													</div>
 												</div>
 
 												<div class="form-group">
 													<label for="descriptif" class="col-lg-2 control-label">Descriptif du meet-up solidaire</label>
 													<div class="col-lg-10">
-														<textarea class="form-control" rows="3" id="descriptif" name="descriptif"></textarea>
+														<textarea class="form-control" rows="3" id="descriptif" name="descriptif" required></textarea>
 														<span class="help-block">Détaillez au mieux les informations et le but !</span>
 													</div>
 												</div>
@@ -68,7 +72,7 @@ require '../includes/nav.php'; ?>
 													<div class="col-lg-10 col-lg-offset-2">
 														<div class="checkbox">
 															<label>
-																<input type="checkbox" name="self"> Recevoir une copie ?
+																<input type="checkbox" name="self" value="1"> Recevoir une copie ?
 															</label>
 														</div>
 														<input type="text" style="position: absolute; left: -9999px; opacity: 0;" id="captcha" name="captcha" placeholder="LAISSEZ CE CHAMP VIDE">
@@ -85,9 +89,31 @@ require '../includes/nav.php'; ?>
 							</div>
 						</div>
 
+
 				<!-- Contenu -->
 				<div class="col-lg-9">
 					<div class="bs-component">
+					
+					<?php
+					if($success) echo '
+						<div class="alert alert-success">
+							<h4>Envoyé !</h4>
+							<p>Merci pour votre message, il a bien été envoyé !</p>
+						</div>
+					';
+					if($bot) echo '
+						<div class="alert alert-warning">
+							<h4>Attention !</h4>
+							<p>Avez-vous bien rempli tout les champs nécessaire ?</p>
+						</div>
+					';
+					if(!empty($error)) echo '
+						<div class="alert alert-danger">
+							<h4>Oh, erreur !</h4> 
+							<p>Revérifiez votre entrée et réessayez. <br><i>Au cas-où, voici l\'erreur : '. $error .'</i></p>
+						</div>
+					';
+					?>
 
 
 						<div class="panel panel-warning">
