96cc42d99cd299431cf727fca666357e95ad6260
MKody <im@kdy.ch>
2015-06-07 16:43:44 +0000

    Update README + fichier d'exemple pour logins

---
+++ includes/logins.exemple.php
@@ -0,0 +1,20 @@
+<?php
+
+// Emails
+$mailUser = "";
+$mailPass = "";
+$mailHost = "";
+$mailPort = "";
+
+// Database
+$dbUser = "";
+$dbPass = "";
+$dbHost = "";
+$dbName = "";
+
+// Administration (mini-site "sang")
+$user = "";
+$password = "";
+
+// Google Developers Console <https://console.developers.google.com/>
+$gcalAPI = "";
